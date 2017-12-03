a644bca4df262260041db0c6a9f012dd78dd56a0
tyommik <2fasti.miem@gmail.com>
2017-12-03 17:30:02 +0300

    add types

--- task_14.py
+++ task_14.py
@@ -1,6 +1,6 @@
 """ вывод слов в обратном порядке. Слова разделены пробелами """
 
-def string_reverse(input_string):
+def string_reverse(input_string: str) -> str:
     words = input_string.split(" ")
     return " ".join([word[::-1] for word in words[::-1]])
 
